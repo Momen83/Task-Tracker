41214aaa69dc41d9d6b314c7242711cc8778ff34
Momen83 <momenhatem2001@gmail.com>
2025-03-16 17:45:22 +0200

    Add Route and About Component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { TasksComponent } from './component/tasks/tasks.component';
+import { AboutComponent } from './component/about/about.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: TasksComponent },  // Default route
+    {path: 'about' , component:AboutComponent}
+  ];
--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,10 @@ import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 import { provideHttpClient  } from '@angular/common/http';
 import {FormsModule} from '@angular/forms';
+import { routes } from './app/app.routes';
+import { provideRouter } from '@angular/router';
 
 bootstrapApplication(AppComponent, {
-  providers: [provideHttpClient(),FormsModule]
+  providers: [provideHttpClient(),FormsModule , provideRouter(routes)]
 })
   .catch(err => console.error(err));
